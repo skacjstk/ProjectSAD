6039dc55381c1fdbf5baa59c1dcae887979c52c1
포도맛 버섯 <skacjstk0818@gmail.com>
2021-05-09 16:28:24 +0900

    asd

---
+++ ChessGameProject/Assets/Scripts/Board/Board.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    //아마 스퀘어셀렉터는 콜라이더 관련해서 통합될 듯 
+    //raycast 관련 행동은 모두 하나로 통합할 예정 
+    //whiteCamera 기준 0,0은 왼쪽 하얀 룩
+    // 7 7은 오른쪽 블랙 룩
+    /*
+     //흰록,흰말,흰비숍,흰퀸,흰킹,흰비숍,흰말,흰록
+     //흰폰 8개 
+     //이후4칸 빔
+     // 검폰 8개
+     //검록,검말,검비숍,검퀸,검킹,검비숍,검말,검록
+     Q: 왜 퀸킹 위치 반대임??    
+     A: 0,0 이 whiteCamera 기준 하얀 록임. 
+     */
+    private const int boardSize = 8;
+    [SerializeField] private ChessGameController theChessGameController;
+    private Piece selectedPiece;
+    
+    
+    public class PieceData
+    {
+       public PieceType pieceType;
+       public TeamColor team;
+    }
+
+    private PieceData[,] BoardInPiece = new PieceData[8,8]; 
+ 
+
+    //위치 정보만 담고 있는 PieceType Board 정보 
+
+
+    private void Start()
+    {
+        //InisializeBoardinPiece()
+    }
+
+    //보드에 기물 위치 정보 초기화 
+    private void InitializeBoardInPiece()
+    {
+        //위치 정보 초기화   
+        //n+1 번째 줄 좌 -> 우 순 n+1 번째 칸 
+        for (int i = 0; i < boardSize; ++i)
+        {
+            //흰폰 초기화 
+            BoardInPiece[1, i].pieceType = PieceType.Pawn;
+            BoardInPiece[1, i].team = TeamColor.White;
+            //검폰 초기화
+            BoardInPiece[6, i].pieceType = PieceType.Pawn;
+            BoardInPiece[6, i].team = TeamColor.Black;
+        } // 폰 위치 초기화 
+        
+        //흰룩 초기화
+        BoardInPiece[0, 0].pieceType = PieceType.Rook;
+        BoardInPiece[0, 0].team = TeamColor.White;
+        BoardInPiece[0, 7].pieceType = PieceType.Rook;
+        BoardInPiece[0, 7].team = TeamColor.White;
+
+        //검룩 초기화
+        BoardInPiece[7, 0].pieceType = PieceType.Rook;
+        BoardInPiece[7, 0].team = TeamColor.Black;
+        BoardInPiece[7, 7].pieceType = PieceType.Rook;
+        BoardInPiece[7, 7].team = TeamColor.Black;
+
+        //흰나
+        BoardInPiece[0, 1].pieceType = PieceType.Knight;
+        BoardInPiece[0, 1].team = TeamColor.White;
+        BoardInPiece[0, 6].pieceType = PieceType.Knight;
+        BoardInPiece[0, 6].team = TeamColor.White;
+        //검나
+        BoardInPiece[7, 1].pieceType = PieceType.Knight;
+        BoardInPiece[7, 1].team = TeamColor.Black;
+        BoardInPiece[7, 6].pieceType = PieceType.Knight;
+        BoardInPiece[7, 6].team = TeamColor.Black;
+        //흰숍
+        BoardInPiece[0, 2].pieceType = PieceType.Bishop;
+        BoardInPiece[0, 2].team = TeamColor.White;
+        BoardInPiece[0, 5].pieceType = PieceType.Bishop;
+        BoardInPiece[0, 5].team = TeamColor.White;
+        //검숍
+        BoardInPiece[7, 2].pieceType = PieceType.Bishop;
+        BoardInPiece[7, 2].team = TeamColor.Black;
+        BoardInPiece[7, 5].pieceType = PieceType.Bishop;
+        BoardInPiece[7, 5].team = TeamColor.Black;
+        //흰퀸
+        BoardInPiece[0, 3].pieceType = PieceType.Queen;
+        BoardInPiece[0, 3].team = TeamColor.White;
+        //검퀸
+        BoardInPiece[7, 3].pieceType = PieceType.Queen;
+        BoardInPiece[7, 3].team = TeamColor.Black;
+
+        //흰킹
+        BoardInPiece[0, 4].pieceType = PieceType.King;
+        BoardInPiece[0, 4].team = TeamColor.White;
+        //검킹
+        BoardInPiece[7, 4].pieceType = PieceType.King;
+        BoardInPiece[7, 4].team = TeamColor.Black;
+    }
+
+    public Vector3Int CalculateCoordsToPosition(Vector2Int coords)
+    {
+        return new Vector3Int(coords.x, 0, coords.y);
+    }
+
+    public int GetBoardSize()
+    {
+        return boardSize;
+    }
+}
---
+++ ChessGameProject/Assets/Scripts/Pieces/Rook.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rook : Piece
+{
+    PieceType pieceType = PieceType.Rook;
+    //    List<Vector2Int> killDirections = new List<Vector2Int>();  //WhiteCamera 기준 z 음수가 앞, x 음수가 오른쪽 ( 이것은 전용함수로 교정할 예정 ) 양파상도 이것으로 판단함
+
+    // Rook 은 직선방향 
+    readonly List<Vector2Int> directions = new List<Vector2Int>() {
+        new Vector2Int(0, 8),
+        new Vector2Int(8, 0),
+        new Vector2Int(0, -8),
+        new Vector2Int(-8, 0),
+    };
+
+
+
+    private void Start()
+    {
+    }
+
+    public override void SelectedPiece()
+    {
+        Debug.Log("SelectedPiece 자식 함수 호출됨: ");
+    }
+    public override PieceType GetPieceType()
+    {
+        return pieceType;
+    }
+
+    //움직일 수 있는 곳을 반환하는 함수 
+    public override bool CanMove(Vector2Int coords)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //실제로 Piece를 움직이게 하는 함수 
+    public override void MovePiece(Vector2Int coords)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<Vector2Int> GetDirections()
+    {
+        return directions;
+    }
+}
