46a94a24a06a84b9799479f74bad22374740706e
포도맛 버섯 <skacjstk0818@gmail.com>
2021-06-06 14:06:01 +0900

    a
    a

--- ChessGameProject/Assets/Scripts/Player.cs
+++ ChessGameProject/Assets/Scripts/Player.cs
@@ -8,10 +8,10 @@ using UnityEngine;
 public class Player : ColliderHandler
 {
     // Start is called before the first frame update
-
+    TeamColor playerTeam = TeamColor.Black;
     void Start()
     {
-
+        CameraSet();
     }
 
     // Update is called once per frame
@@ -20,6 +20,17 @@ public class Player : ColliderHandler
         if(Input.GetMouseButtonDown(0))
         {
             SelectPiece();
+        }
+    }
+
+    private void CameraSet()
+    {
+        //BlackTeam 을할당 받았을 경우. 카메라 위치를 검은색으로 바꾸기 
+        if (playerTeam.Equals(TeamColor.Black))
+        {
+            Debug.LogWarning("실행됨 카메라셋");
+            playerCamera.transform.position = new Vector3(3.5f, 8f, 8f);
+            playerCamera.transform.Rotate(130, -180, 0);
         }
     }
 }
